3abf61fc8532f6f80bbd023cf614a2db9e23360c
j <j@localhost.com>
2020-05-17 02:20:26 -0500

    adding manage.py command all_sources to trigger a POST request of all the DBs Source objects data to map-the-news

--- sifter/models.py
+++ sifter/models.py
@@ -41,8 +41,7 @@ class Category(db.Model):
     __tablename__ = 'category'
 
     id = db.Column(db.Integer(), primary_key=True)
-    name = db.Column(db.String(50), nullable=False, blank=False, unique=False)
-
+    name = db.Column(db.String(50), nullable=False, unique=True)
 
     @property
     def json(self):
