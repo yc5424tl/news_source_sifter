243d8594e74ac44b442474f613f3bb3de99b2fc3
yc5424tl <yc5424tl@go.minneapolis.edu>
2020-05-22 07:51:38 -0500

    removed generators, simplified overall naming and structure

--- sifter_app.py
+++ sifter_app.py
@@ -1,11 +1,10 @@
 from sifter import create_app, scheduler, db
 import os
 
-
+import time
 import json
 import random
 import requests
-import time
 import logging
 
 api_key = os.getenv("NEWS_SRC_MS_API_KEY")
@@ -226,58 +225,28 @@ NOT_ENTERED_AT_ALL = {
 def sift_sources():
 
     with app.app_context():
-
-        if verify_base_categories() and verify_base_sources():
+        if send_all_sources:
             countries = list(api_country_codes.keys())
             random_country = random.choice(countries)
-            print(f'random country = {random_country}')
-
             random_category = random.choice(categories)
-            print(f'random category = {random_category}')
-            sources_for_country = request_country_sources(
+            sources_for_country = req_country_src_data(
                 alpha2_code=random_country, src_cat=random_category
             )
             if sources_for_country:
-                src_ids = build_country_sources(
-                    generated_country_sources=sources_for_country,
+                src_update = build_country_src_data(
+                    src_data=sources_for_country,
                     alpha2_code=random_country,
                     src_cat=random_category,
                 )
-                print(f'src_ids in sift_sources being sent to id_set_to_json = {src_ids}')
-                json_payload = id_set_to_json(src_ids)
-                print(f'payload being sent to send_payload in sift_sources ->')
-                print(f'{json_payload}')
-                send_payload(json_payload)
-                return True
-            else:
-                print('returning false from sift sources')
-                return False
+                if send_payload(src_update):
+                    print('sifter payload delivered')
+                    return True
+        print('returning false from sift sources')
         return False
-
-
-def send_all_sources():
-    print('top of all sources')
-    if verify_base_categories() and verify_base_sources():
-        print('inside verifies of send all sources')
-        sources = Source.query.all()
-        all_sources = {"sources": [source.json for source in sources]}
-        try:
-            print('about to send payload from send all sources -->')
-            print(f'PAYLOAD = {all_sources}')
-
-            if send_payload(all_sources):
-                return True
-        except ConnectionError:
-            logger.log(
-                level=logging.INFO, msg="Connection Error while delivering payload."
-            )
-    print('returning false from send all sources')
-    return False
 
 
 print("creating app")
 app = create_app()
-# scheduler.add_job(id="send_all_src", func=send_all_sources)
 scheduler.add_job(
     id="sifter_scheduler", func=sift_sources, trigger="interval", minutes=6
 )
@@ -299,21 +268,7 @@ def stay_alive():
     return json.dumps({"stay": "alive"}), 200, {"ContentType": "application/json"}
 
 
-def id_set_to_json(id_set: set):
-    json_payload = {"sources": [] for x in range(2)}
-    print(f'initial payload = {json_payload} ')
-    for src_id in id_set:
-        source = Source.query.filter_by(id=src_id).first()
-        print(f'source.json to add to payload = {source.json}')
-        json_payload["sources"].append(source.json)
-    print(f'payload returning from id_set_to_json = {json_payload}')
-    return json_payload
-
-
-def send_payload(payload: dict):
-    print('=====================================PAYLOAD==============================================')
-    if payload['sources']:
-        print((payload['sources'])[:3])
+def send_payload(payload: {{}}):
     login_url = os.getenv("NEWS_MAP_LOGIN_URL")
     username = os.getenv("NEWS_MAP_POST_USER")
     password = os.getenv("NEWS_MAP_POST_PW")
@@ -329,226 +284,172 @@ def send_payload(payload: dict):
         "csrfmiddlewaretoken": csrf_token,
         "next": "/",
     }
+    try:
+        r1 = client.post(login_url, data=login_data, headers=dict(Referer=login_url))
+        logger.log(level=logging.INFO, msg=f"response_1 => {r1}")
+        r2 = client.post(url=post_url, json=payload)
+        logger.log(level=logging.INFO, msg=f"response_2 => {r2}")
+        return True
+    except ConnectionError:
+        logger.log(
+            level=logging.INFO, msg="Connection Error while delivering payload."
+        )
+        return False
 
-    r1 = client.post(login_url, data=login_data, headers=dict(Referer=login_url))
-    logger.log(level=logging.INFO, msg=f"response_1 => {r1}")
-    print(f'\n\n=====================PAYLOAD LOGIN RESPONSE======================= {r1}\n\n')
-    r2 = client.post(url=post_url, json=payload)
-    logger.log(level=logging.INFO, msg=f"response_2 => {r2}")
-    print(f'\n\n==========================POST PAYLOAD RESPONSE========================== {r2}\n\n')
-    # time.sleep(360)
-    print('\n=========================END PAYLOAD=============================\n\n')
-    return True
+
+def send_all_sources():
+    print('top of all sources')
+    if verify_base_cat() and verify_base_src():
+        sources = Source.query.all()
+        all_src_update = {src.json for src in sources}
+        if send_payload(all_src_update):
+            print('all_sources payload delivered')
+            return True
+    print('returning false from send all sources')
+    return False
 
 
-def verify_base_categories():
-    for category in categories:
-        category = Category.query.filter_by(name=category).first()
-        if not category:
-            new_category = Category(name=category)
-            db.session.add(new_category)
+def verify_base_cat():
+    for cat in categories:
+        cat = Category.query.filter_by(name=cat).first()
+        if not cat:
+            new_cat = Category(name=cat)
+            db.session.add(new_cat)
     db.session.commit()
-    print('verified base categories')
     return True
 
 
-def verify_base_sources():
-    first_source = Source.query.filter_by(id=1).first()
-    if not first_source:
-        new_sources = set()
+def verify_base_src():
+    first_src = Source.query.filter_by(id=1).first()
+    if not first_src:
+        src_update = set()
         try:
             with open("./static/js/top_sources.json") as json_data:
-                source_data = json.load(json_data)["sources"]
-                for source in source_data:
-                    new_source_category = Category.query.filter_by(
-                        name=source["category"]
+                src_data = json.load(json_data)["sources"]
+                for src in src_data:
+                    new_src_cat = Category.query.filter_by(
+                        name=src["category"]
                     ).first()
                     new_source = Source(
-                        name=source["name"],
-                        country=source["country"],
-                        language=source["language"],
-                        url=source["url"],
-                        categories=[new_source_category],
+                        name=src["name"],
+                        country=src["country"],
+                        language=src["language"],
+                        url=src["url"],
+                        categories=[new_src_cat],
                     )
                     db.session.add(new_source)
                     db.session.commit()
-                    new_sources.add(new_source.id)
-                json_payload = id_set_to_json(new_sources)
-                print('verified base sources, about to send payload after building from file')
-                if send_payload(json_payload):
-                    print('sent payload after building base sources from file')
+                    src_update.add(new_source.json)
+                if send_payload(src_update):
                     return True
-            print('returning catch all false from verify base sources from file')
+            logger.log(level=logging.INFO, msg='Error sending payload from file in verify_base_src()')
             return False
-
         except FileNotFoundError:
-            logger.log(level=logging.INFO, msg="Error Building Base Sources from File")
-            top_src_data = request_top_sources()
-            if top_src_data:
-                top_source_ids = build_top_sources(top_src_data)
-                json_payload = id_set_to_json(top_source_ids)
-                print('verified base sources via request, about to send payload')
-                if send_payload(json_payload):
-                    print('sent payload from verified base sources filenotfoundexception')
-                    print(f'payload = \n\n{json_payload}\n\n')
+            logger.log(level=logging.INFO, msg="Unable to locate file in verify_base_src()")
+            src_data = req_top_src_data()
+            if src_data:
+                src_update = build_top_src_data(src_data)
+                if send_payload(src_update):
+                    scheduler.pause()
+                    print('paused scheduler, waiting six minutes')
+                    time.sleep(360)
+                    scheduler.resume()
+                    print('resumed scheduler')
                     return True
-            print('returning false inside filenotfounderror inside verified base sources')
+            logger.log(level=logging.INFO, msg='No data returned from req_top_src_data in verify_base_src')
             return False
     else:
-        print('verified base sources already present in verified base sources')
         return True
 
 
-def request_country_sources(alpha2_code, src_cat=None):
-
-    key = os.getenv('NEWS_SRC_MS_API_KEY')
-    print(f'key = {key}')
-    print(f'alpha2_code = {alpha2_code}')
-    print(f'src_cat = {src_cat}')
+def req_country_src_data(alpha2_code, src_cat=None):
     if src_cat is None:
-        endpoint = f"https://newsapi.org/v2/top-headlines?country={alpha2_code}&apiKey={key}"
+        endpoint = f"https://newsapi.org/v2/top-headlines?country={alpha2_code}&apiKey={api_key}"
     else:
-        endpoint = f"https://newsapi.org/v2/top-headlines?country={alpha2_code}&category={src_cat}&apiKey={key}"
-    print(f'endpoint = {endpoint}')
+        endpoint = f"https://newsapi.org/v2/top-headlines?country={alpha2_code}&category={src_cat}&apiKey={api_key}"
     response = requests.get(endpoint)
-    print(f'CODE: {response.status_code}\nTEXT: {response.text}')
-    print(f'\n\nRESPONSE = \n{response}\n\n')
-    print(f'\n\nRESPONSE.json =\n{response.json}')
-
     if response.json()["status"] == "ok":
-        data = response.json()["articles"]
-        print('\n\nRESPONSE DATA[articles] =\n\n{data}')
-        data_gen = iter(data)
-        print('returning generated sources in request_country_sources')
-        return generated_sources(data_gen)
-
-
+        return response.json()["articles"]
     elif response.json()["status"] == "error":
         logger.log(
             level=logging.ERROR,
             msg=f'Error Code: {response.json()["code"]} Message: {response.json()["message"]}',
         )
-        print('returning none in request country sources')
-        return None
-    else:
-        print('returning 2nd none in request country sources')
         return None
 
 
-def build_country_sources(generated_country_sources, alpha2_code, src_cat):
-    print('\n\nTOP OF BUILD_COUNTRY_SOURCES\n\n')
-    new_and_updated_source_ids = set()
-    category = Category.query.filter_by(name=src_cat).first()
-
-    if category is None:
-        category = Category(name=src_cat)
-        db.session.add(category)
+def build_country_src_data(src_data, alpha2_code, src_cat):
+    src_update = set()
+    cat = Category.query.filter_by(name=src_cat).first()
+    if cat is None:
+        cat = Category(name=src_cat)
+        db.session.add(cat)
         db.session.commit()
-
-    count = 0
-    for src in generated_country_sources:
-        print(f'have src {count} from generator in build_country_sources')
-        source = Source.query.filter_by(
-            name=src["source"]["name"]
+    for art in src_data:
+        src = Source.query.filter_by(
+            name=art["source"]["name"]
         ).first()  # check if source in db
-        count += 1
-
-
-        print(f'src from generator iteration in build_country_sources = {src}')
-
-        if source is None:  # Source not in DB
-            new_source = Source(
-                name=src["source"]["name"],
+        if src is None:  # Source not in DB
+            new_src = Source(
+                name=art["source"]["name"],
                 country=alpha2_code,
                 language=country_codes.get(alpha2_code).get("language"),
             )
-            new_source.categories.append(category)
-            db.session.add(new_source)
+            new_src.categories.append(cat)
+            db.session.add(new_src)
             db.session.commit()
-            print('adding new source to set in build country sources')
-            print(f'new source being added = {new_source}')
-            new_and_updated_source_ids.add(new_source.id)
-            print(f'new_and_updated_source_ids after adding = {new_and_updated_source_ids}')
-
+            src_update.add(new_src.json)
         else:  # Source already in DB
-            print('source in build_country_sources already in db')
             try:
-                idx = source.categories.index(
-                    category
-                )  # Check if category currently listed for source
+                idx = src.categories.index(cat)  # Check if category currently listed for source
             except ValueError:  # Exception Raised if category not listed
-                source.categories.append(category)
+                src.categories.append(cat)
                 db.session.commit()
-                print('addind source w new cat to set in build country sources')
-                new_and_updated_source_ids.add(source.id)
+                src_update.add(src.json)
     db.session.commit()
-    print('finished building country sources')
-    print(f'new_and_updated_source_ids in build_country_sources = {new_and_updated_source_ids}')
-    print('\n\nBOTTOM OF BUILD_COUNTRY_SOURCES\n\n')
-    return new_and_updated_source_ids
+    return src_update
 
 
-def request_top_sources():
+def req_top_src_data():
     response = requests.get(f"https://newsapi.org/v2/sources?apiKey={api_key}")
-
     if response.json()["status"] == "ok":
-        data = response.json()["sources"]
-        top_sources_gen = iter(data)
-        print('returning gen sources in request top sources')
-        return generated_sources(top_sources_gen)
-
+        return response.json()["sources"]
     elif response.json()["status"] == "error":
         logger.log(
             level=logging.ERROR,
             msg=f'Code: {response.json()["code"]}, Message: {response.json()["message"]}',
         )
-        print('returning none in request top sources')
         return None
 
 
-def build_top_sources(generated_top_sources):
-
-    new_and_updated_id_set = (
-        set()
-    )  # Store ID of each new Source and Sources with a new Category.
-
-    for src in generated_top_sources:
-        print(f'source from gen_top_sources iteration in build_top_sources')
-        category = Category.query.filter_by(
+def build_top_src_data(src_data):
+    src_update = set()
+    for src in src_data:
+        cat = Category.query.filter_by(
             name=src["category"]
         ).first()  # Check DB for Category[
-        if category is None:  # Category does not exist in DB, add Category
-            category = Category(name=src["category"])
-            db.session.add(category)
+        if cat is None:  # Category does not exist in DB, add Category
+            cat = Category(name=src["category"])
+            db.session.add(cat)
             db.session.commit()
-
-        source = Source.query.filter_by(
+        src = Source.query.filter_by(
             name=src["name"]
         ).first()  # Checking DB for Source
-        if source is None:  # Source does not exist in DB, add Source.
-            source = Source(
+        if src is None:  # Source does not exist in DB, add Source.
+            new_src = Source(
                 name=src["name"], country=src["country"], language=src["language"], url=src['url']
             )
-            source.categories.append(category)
-            db.session.add(source)
+            new_src.categories.append(cat)
+            db.session.add(new_src)
             db.session.commit()
-            print(f'new_top_source_id for set == {source.id}')
-            new_and_updated_id_set.add(source.id)
-            print(f'after adding source.id, set = {new_and_updated_id_set}')
+            src_update.add(new_src.json)
         else:  # Source exists in DB.
-            print('source exists, checking category in build_top_sources')
             try:  # Check Source for Category
-                idx = source.categories.index(category)
+                idx = src.categories.index(cat)
             except ValueError:  # Category does not exist for Source, add Category.
-                source.categories.append(category)
+                src.categories.append(cat)
                 db.session.commit()
-                print('adding already present source to nauis in build_top_sources')
-                new_and_updated_id_set.add(source.id)
-
+                src_update.add(src.json)
     db.session.commit()
-    print('returning id set in build top sources')
-    return new_and_updated_id_set
+    return src_update
 
-
-def generated_sources(src_gen):
-    yield from src_gen
