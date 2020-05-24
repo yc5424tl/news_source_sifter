d7d1812b798bdb07ff6d89390a5937325a719286
j <j@localhost.com>
2020-05-24 05:17:06 -0500

    adding manage.py command all_sources to trigger a POST request of all the DBs Source objects data to map-the-news

--- sifter_app.py
+++ sifter_app.py
@@ -321,9 +321,9 @@ def send_all_sources():
         print('verify_base_cat() and verify_base_src() @ send_all_sources')
         sources = Source.query.all()
         print(f'type(sources) == {type(sources)}')
-        print(f'sources.length() == {sources.len()}')
+        print(f'sources.length() == {Source.query.all().len()}')
         src_update = set()
-        for src in sources:
+        for src in Source.query.all():
             print(f'src.json for src_update.add(src.json) == {src.json}')
             src_update.add(src.json)
 
